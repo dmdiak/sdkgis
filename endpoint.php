912a94eaebf9e179417fb6f1a4ab4f0a6c463018
dmdiak <dm.diak@seznam.cz>
2017-07-11 10:59:18 +0200

    GisApi: refactor, +initGame, +initDemoGame

--- endpoint.php
+++ endpoint.php
@@ -7,3 +7,4 @@ use SdkGis\GisApi;
 $config = include('src/config/config.php');
 $gisApi = new GisApi($config);
 
+var_dump($gisApi->initDemoGame('38651906921e4aec72793984a91127bfdc300885'));
--- src/GisApi.php
+++ src/GisApi.php
@@ -30,10 +30,10 @@ class GisApi
      */
     private function getAuthHeaders($requestParams = [])
     {
-        $integrationData = $this->config['integration_data'];
+        $integrationData = $this->config['integrationData'];
 
         $authHeaders = [
-            'X-Merchant-Id' => $integrationData['merchant_id'],
+            'X-Merchant-Id' => $integrationData['merchantId'],
             'X-Timestamp'   => time(),
             'X-Nonce'       => md5(uniqid(mt_rand(), true)),
         ];
@@ -43,26 +43,27 @@ class GisApi
         ksort($mergedParams);
         $hashString = http_build_query($mergedParams);
 
-        $authHeaders['X-Sign'] = hash_hmac('sha1', $hashString, $integrationData['merchant_key']);
+        $authHeaders['X-Sign'] = hash_hmac('sha1', $hashString, $integrationData['merchantKey']);
 
         return $authHeaders;
     }
 
     /**
      * GIS API
-     * method: /games
+     * Retrieving games list.
+     * Method: /games
      * @return array
      */
     public function getGames()
     {
         $authHeaders = $this->getAuthHeaders();
 
-        $integrationData = $this->config['integration_data'];
-        $gisApiOpt = $this->config['gis_api_opt'];
+        $integrationData = $this->config['integrationData'];
+        $gisApiOpt = $this->config['gisApiOpt'];
 
-        $curl = curl_init($integrationData['base_api_url'] . '/games');
+        $curl = curl_init($integrationData['baseApiUrl'] . '/games');
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, $gisApiOpt['connect_timeout']);
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, $gisApiOpt['connectTimeout']);
         curl_setopt($curl, CURLOPT_TIMEOUT, $gisApiOpt['timeout']);
 
         $headers = [];
@@ -79,7 +80,8 @@ class GisApi
 
     /**
      * GIS API
-     * method: /games/lobby
+     * Returns list of tables for the selected game.
+     * Method: /games/lobby
      * @param string $gameUuid
      * @param string $currency
      * @return array
@@ -94,12 +96,12 @@ class GisApi
 
         $authHeaders = $this->getAuthHeaders($requestParams);
 
-        $integrationData = $this->config['integration_data'];
-        $gisApiOpt = $this->config['gis_api_opt'];
+        $integrationData = $this->config['integrationData'];
+        $gisApiOpt = $this->config['gisApiOpt'];
 
-        $curl = curl_init($integrationData['base_api_url'] . '/games/lobby?' . $requestParamsStr);
+        $curl = curl_init($integrationData['baseApiUrl'] . '/games/lobby?' . $requestParamsStr);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, $gisApiOpt['connect_timeout']);
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, $gisApiOpt['connectTimeout']);
         curl_setopt($curl, CURLOPT_TIMEOUT, $gisApiOpt['timeout']);
 
         $headers = [];
@@ -114,9 +116,111 @@ class GisApi
         return $result;
     }
 
-    public function initGame()
-    {
+    /**
+     * GIS API
+     * Initializing game session.
+     * Method: games/init
+     * @param string $gameUuid
+     * @param string $playerId
+     * @param string $playerName
+     * @param string $currency
+     * @param string $sessionId
+     * @param null|string $returnUrl [optional]
+     * @param null|string $language [optional]
+     * @param null|string $email [optional]
+     * @param null|string $lobbyData [optional]
+     * @return array
+     */
+    public function initGame(
+        $gameUuid,
+        $playerId,
+        $playerName,
+        $currency,
+        $sessionId,
+        $returnUrl = null,
+        $language = null,
+        $email = null,
+        $lobbyData = null
+    ) {
+        $requestParams = [
+            'game_uuid' => $gameUuid,
+            'player_id' => $playerId,
+            'player_name' => $playerName,
+            'currency' => $currency,
+            'session_id' => $sessionId,
+            'return_url' => $returnUrl,
+            'language' => $language,
+            'email' => $email,
+            'lobby_data' => $lobbyData,
+        ];
 
+        $authHeaders = $this->getAuthHeaders($requestParams);
+
+        $integrationData = $this->config['integrationData'];
+        $gisApiOpt = $this->config['gisApiOpt'];
+
+        $curl = curl_init($integrationData['baseApiUrl'] . '/games/init');
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($requestParams));
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, $gisApiOpt['connectTimeout']);
+        curl_setopt($curl, CURLOPT_TIMEOUT, $gisApiOpt['timeout']);
+
+        $headers = [
+            'Content-Type: application/x-www-form-urlencoded',
+        ];
+        foreach ($authHeaders as $key => $value) {
+            $headers[] = $key . ': ' . $value;
+        }
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+        $json = curl_exec($curl);
+        $result = json_decode($json, true);
+
+        return $result;
+    }
+
+    /**
+     * GIS API
+     * Initializing demo game session (only if provider has demo mode).
+     * Method: games/init-demo
+     * @param string $gameUuid
+     * @param null|string $returnUrl [optional]
+     * @param null|string $language [optional]
+     * @return array
+     */
+    public function initDemoGame($gameUuid, $returnUrl = null, $language = null)
+    {
+        $requestParams = [
+            'game_uuid' => $gameUuid,
+            'return_url' => $returnUrl,
+            'language' => $language,
+        ];
+
+        $authHeaders = $this->getAuthHeaders($requestParams);
+
+        $integrationData = $this->config['integrationData'];
+        $gisApiOpt = $this->config['gisApiOpt'];
+
+        $curl = curl_init($integrationData['baseApiUrl'] . '/games/init-demo');
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($requestParams));
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, $gisApiOpt['connectTimeout']);
+        curl_setopt($curl, CURLOPT_TIMEOUT, $gisApiOpt['timeout']);
+
+        $headers = [
+            'Content-Type: application/x-www-form-urlencoded',
+        ];
+        foreach ($authHeaders as $key => $value) {
+            $headers[] = $key . ': ' . $value;
+        }
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+        $json = curl_exec($curl);
+        $result = json_decode($json, true);
+
+        return $result;
     }
 
 }
